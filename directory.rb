cbfc5a601eb1e753d180abed15d07e996d36549f
BecaLParker <becaparker@gmail.com>
2021-04-21 17:02:31 +0100

    Adds interactive menu in pseudocode

--- directory.rb
+++ directory.rb
@@ -1,3 +1,27 @@
+def interactive_menu
+  loop do
+    #1. print the menu and askt he user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    #2. read the input menu and save it to the variable
+    selection = gets.chomp
+    #3. do what the user has asked
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        #show the students
+      when "9"
+        Exit
+      else
+        puts "I don't known what you meant, try again"
+        
+      end
+    end
+  end
+end
+
 # Let's put all the students into an array
 # # students = [
 #   { name: 'Dr. Hannibal Lector', cohort: :november },
