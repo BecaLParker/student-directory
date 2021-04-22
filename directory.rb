426028f36077ed75272e264164951a7a9c294ca8
BecaLParker <becaparker@gmail.com>
2021-04-22 08:59:31 +0100

    Fixes the input errors in input_students

--- directory.rb
+++ directory.rb
@@ -1,55 +1,41 @@
 def interactive_menu
   loop do
-    #1. print the menu and askt he user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    #2. read the input menu and save it to the variable
+    puts '1. Input the students'
+    puts '2. Show the students'
+    puts '9. Exit'
+    # 2. read the input menu and save it to the variable
     selection = gets.chomp
-    #3. do what the user has asked
+    # 3. do what the user has asked
     case selection
-      when "1"
-        students = input_students
-      when "2"
-        #show the students
-      when "9"
-        Exit
-      else
-        puts "I don't known what you meant, try again"
-        
-      end
+    when '1'
+      students = input_students
+    when '2'
+      # show the students
+    when '9'
+      exit
+    else
+      puts "I don't known what you meant, try again"
     end
   end
 end
 
-# Let's put all the students into an array
-# # students = [
-#   { name: 'Dr. Hannibal Lector', cohort: :november },
-#   { name: 'Darth Vader', cohort: :november },
-#   { name: 'Nurse Ratched', cohort: :november },
-#   { name: 'Michael Corleone', cohort: :november },
-#   { name: 'Alex DeLarge', cohort: :november },
-#   { name: 'The Wicked Witch of the West', cohort: :november },
-#   { name: 'Terminator', cohort: :november },
-#   { name: 'Freddy Krueger', cohort: :november },
-#   { name: 'The Joker', cohort: :november },
-#   { name: 'Joffrey Baratheon', cohort: :november },
-#   { name: 'Norman Bates', cohort: :november }
-# ]
 def input_students
   puts 'Please enter the name and cohort of the students in this format: name, cohort'
   puts 'To finish, just hit return twice'
   # create an empty array
   students = []
   # get the first name
-  name, cohort = gets.chomp.split(',')
+
+  input = gets.chomp
+
   # while the araay is not empty, repeat this code
-  until name.empty? || cohort.empty?
+  until input.empty?
     # add the student hash to the array
+    name, cohort = input.split(',')
     students << { name: name, cohort: cohort }
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name, cohort = gets.chomp.split(',')
+    input = gets.chomp
   end
   # return the array of students
   students
@@ -72,6 +58,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students".center(40)
 end
 # nothing happens until we call the methods
+interactive_menu
 students = input_students
 print_header
 print(students)
